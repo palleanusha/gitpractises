77c1a651e17a1a2c45e7ea66490b6b81c7dd42e4
palleanusha <palleanusha90@gmail.com>
2021-12-19 13:53:13 +0530

    "dsdf"

--- src/test/java/Gitssample.java
+++ src/test/java/Gitssample.java
@@ -3,7 +3,8 @@ public class Gitssample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("first test");
-	}
+System.out.println("second  test");
 
-}
+}}
+
+
